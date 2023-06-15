e358d9f4190407d8959495d2e234557a1c3f52e5
imchetan9 <chetanlokhande111@gmail.com>
2023-06-15 18:28:38 +0530

    first commit

---
+++ springbootdemoapp/src/main/java/com/springbootdemoapp/app/SpringbootdemoappApplication.java
@@ -0,0 +1,14 @@
+package com.springbootdemoapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring boot Start");
+		SpringApplication.run(SpringbootdemoappApplication.class, args);
+	}
+
+}
